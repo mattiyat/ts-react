eb92cde47491ca41e874ad26d4525f19febf1a3f
mattiyat <mattt412@gmail.com>
2022-01-11 09:45:52 -0500

    small dummy data changes

--- src/pages/assetList/AssetList.tsx
+++ src/pages/assetList/AssetList.tsx
@@ -7,7 +7,7 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid'
 // }
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'id', headerName: 'Serial', width: 100 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
@@ -26,15 +26,15 @@ const columns: GridColDef[] = [
 ]
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 900394493, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 900439444, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 900305833, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 900394493, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 900392023, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 90403393, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 900394322, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 900394493, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 900390873, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ]
 
 export default function AssetList() {
